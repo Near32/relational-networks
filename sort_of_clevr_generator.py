93a725a45c91d3f946d1a6fa1a378d22344d1451
Kevin Denamganai <denamganai.kevin@gmail.com>
2018-12-17 17:55:20 +0000

    changed data dir

--- sort_of_clevr_generator.py
+++ sort_of_clevr_generator.py
@@ -13,7 +13,7 @@ question_size = 11 ##6 for one-hot vector of color, 2 for question type, 3 for q
 """Answer : [yes, no, rectangle, circle, r, g, b, o, k, y]"""
 
 nb_questions = 10
-dirs = './data'
+dirs = './DATASETS'
 
 colors = [
     (0,0,255),##r
